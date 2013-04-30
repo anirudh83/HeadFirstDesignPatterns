6bc60d4e5c4c6df47c98eb7fc95d64aa26da3b33
Anirudh <anirudh.bh@gmail.com>
2013-04-30 15:29:12 +0530

    Second

--- src/com/anirudh/practice/LightOnCommand.java
+++ src/com/anirudh/practice/LightOnCommand.java
@@ -1,9 +1,17 @@
 package com.anirudh.practice;
 
+/**
+ * 
+ * @author anirudh
+ *
+ */
 public class LightOnCommand implements Command{
 	
 	private Light light;
-	
+
+	public LightOnCommand(Light light){
+		this.light=light;
+	}
 	
 	@Override
 	public void execute() {
